8f95966ca389e8d15d4aa3e948380ea00307dcff
mainairungu <irungu.m.stephen@gmail.com>
2023-02-20 00:49:51 -0800

    Corrected the code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
-
-/*This code returns a string*/
+/**
+ * the code is as follows
+ * This code returns a string*/
 int main(void)
 {
 	char str2[] = "\"Programming is like building a multilingual puzzle";
 	char str1[] = "";
-	
 	puts(str2);
 	puts(str1);
 	return (0);
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - Entry point
+ * About the piece of art Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	
 	return (0);
 }
