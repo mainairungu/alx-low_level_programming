8e60c3b0255be9aeaa497d7441cce62735ccb672
mainairungu <irungu.m.stephen@gmail.com>
2023-02-20 01:13:23 -0800

    Question 6 changes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * About the piece of art Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)\nSize of an int: 4 byte(s)\nSize of a long int: 4 byte(s)\nSize of a long long int: 8 byte(s)\nSize of a float: 4 byte(s)");
+	return (0);
+}
