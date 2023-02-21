8fcf817140746d7a0be7cfbfda81daef67f960e8
mainairungu <irungu.m.stephen@gmail.com>
2023-02-21 08:52:37 -0800

    shf

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - check the code
  * Return: Always 0.
@@ -8,16 +8,4 @@ int main(void)
     void print_alphabet(void);
     print_alphabet();
     return (0);
-}
-void print_alphabet(void)
-{
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-	{
-		putchar(c);
-	}
-
-	putchar('\n');
-        return;
 }
